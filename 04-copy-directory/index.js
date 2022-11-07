64f389b76a1465da0208c10a981d8de0b05b55be
tglsp <tglspfdsfgs@gmail.com>
2022-11-07 18:33:46 +0200

    feat: complete copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,15 @@
+const promises = require('fs/promises');
+const fs = require('fs');
+const path = require('path');
 
+(async function() {
+  try {
+    const createDir = await promises.mkdir(path.resolve(process.argv[1], 'files-copy'), { recursive: true });
+    const files = await promises.readdir(path.resolve(process.argv[1], 'files'));
+    for (const file of files) {
+      await promises.copyFile(path.resolve(process.argv[1], 'files', file), path.resolve(process.argv[1], 'files-copy', file));
+    }
+  } catch (err) {
+    console.error(err.message);
+  }
+})();
